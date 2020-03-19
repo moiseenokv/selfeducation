899989c746491c505974fc2ccf4c64b9ca777ab9
Vitali Maisiayonak <moiseenokv@gmail.com>
2020-03-19 19:10:04 +0300

    delete old webpack task

--- webpack-task/src/js/index.js
+++
@@ -1,9 +0,0 @@
-require('../css/style.css');
-let people = (require('./people.js'));
-let $ = require('jquery');
-
-$('body').append('<ul></ul>');
-
-people.forEach(function(item){
-    $('ul').append("<li>"+item.name+"</li>");
-});
